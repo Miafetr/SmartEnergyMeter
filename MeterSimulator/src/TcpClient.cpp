be75a43827047a44123fac563dd272f630333d5d
Mia Fetr <mahshidfetr@gmail.com>
2026-07-29 14:41:05 -0700

    Add detailed telemetry packet output

--- MeterSimulator/src/TcpClient.cpp
+++ MeterSimulator/src/TcpClient.cpp
@@ -1,5 +1,8 @@
 #include "TcpClient.h"
 
+#include <iomanip>
+#include <iostream>
+
 TcpClient::TcpClient()
 {
     connected = false;
@@ -17,6 +20,52 @@ bool TcpClient::sendTelemetry(const TelemetryData& packet)
     {
         return false;
     }
+
+    std::cout << std::fixed << std::setprecision(2);
+
+    std::cout << "\n========================================\n";
+    std::cout << "       Smart Meter Telemetry Packet\n";
+    std::cout << "========================================\n";
+
+    std::cout << "Device ID       : "
+              << packet.deviceId << '\n';
+
+    std::cout << "Timestamp       : "
+              << packet.timestamp << '\n';
+
+    std::cout << "Sequence Number : "
+              << packet.sequenceNumber << '\n';
+
+    std::cout << "Voltage         : "
+              << packet.voltage << " V\n";
+
+    std::cout << "Current         : "
+              << packet.current << " A\n";
+
+    std::cout << "Power           : "
+              << packet.power << " W\n";
+
+    std::cout << "Temperature     : "
+              << packet.temperature << " C\n";
+
+    std::cout << "Status          : ";
+
+    switch (packet.status)
+    {
+        case DeviceStatus::Online:
+            std::cout << "Online";
+            break;
+
+        case DeviceStatus::Warning:
+            std::cout << "Warning";
+            break;
+
+        case DeviceStatus::Fault:
+            std::cout << "Fault";
+            break;
+    }
+
+    std::cout << "\n========================================\n";
 
     return true;
 }
